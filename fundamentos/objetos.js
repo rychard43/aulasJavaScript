66a750fd8488db8bbe8f22d405c941fe541ebd1f
Rychard souza <rychard34@outlook.com>
2018-12-04 10:13:31 -0300

    aulas js

---
+++ fundamentos/objetos.js
@@ -0,0 +1,19 @@
+const prod1 = {}
+prod1.nome = 'lumia 640'
+prod1.valor = 700.00
+
+console.log(prod1)
+
+
+const prod2 ={
+    nome:'camisa preta',
+    preco:79.90,
+    //objeto dentro de outro
+    obj:{
+        exemplo:1
+
+    }
+    
+}
+
+console.log(prod2)
