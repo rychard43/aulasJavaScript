f75f01c010682f24f8560840e4340af081bc0e56
Rychard souza <rychard34@outlook.com>
2019-06-17 09:13:31 -0300

    aulas js (fim do capitulo array)

---
+++ array/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Katarina','Morgan']
+const filhos = ['Rychard','Ricardo']
+
+const todos = filhas.concat(filhos)
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2],[3,4],5,[[6,7]]))
