cd9ac2e64456a8c9a7ed2d0f917af2e02a7a97e7
rychard43 <rychard34@outlook.com>
2019-12-17 16:29:35 -0300

    inicianto react

---
+++ react/exercicios/src/componentes /BomDIa.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => 
+//Fragment
+<>
+    <h1>Bom dia {props.nome} idade {props.idade}</h1>
+    <h2>Ola</h2>
+</>
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import BomDIa from './componentes /BomDIa'
+
+
+ReactDom.render(<BomDIa nome="Rychard" idade={21} />, document.getElementById('root'))
